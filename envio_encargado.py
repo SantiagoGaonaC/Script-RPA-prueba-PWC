83ddee8ffb93f64f8c184067379982d34d0b8bc7
vudumstead <santiago.gaona.2018@upb.edu.co>
2022-10-22 11:57:47 -0500

    Solución - Spawm gmail 90seg

--- envio_encargado.py
+++ envio_encargado.py
@@ -13,39 +13,53 @@ import ssl
 import smtplib
 import datetime
 from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart #info-asunto-formato del mensaje etc...
+from email.mime.multipart import MIMEMultipart
+from time import time #info-asunto-formato del mensaje etc...
 from str_html import html_estructura
+import time
 
 def envio_email_encargado(df,email_usuario_e,contraseña_usuario_e,email_encargado):
+    contador = 0
+    # cantidad de yes = print(df['Notificaciones'].value_counts().to_frame())
     for i in df.loc[df['Notificaciones']=='Yes'].index:
-        fecha_p = (df['Dia*'][i])
-        fecha_s = df['Día'][i].strftime('%A')+", " + df['Día'][i].strftime('%d de %B de %Y')
-        asunto = (f"""*Notificación sobre Pedido {df['Codigo_recepcion'][i]} {fecha_p}""")
+        contador += 1 
+        if contador != 70:
+            test(df,email_usuario_e,contraseña_usuario_e,email_encargado,i,contador)
+        elif contador == 70:
+            time.sleep(90)
+            contador == 0
+            test(df,email_usuario_e,contraseña_usuario_e,email_encargado,i,contador)
+
+def test(df,email_usuario_e,contraseña_usuario_e,email_encargado,i,contador):
+    fecha_p = (df['Dia*'][i])
+    fecha_s = df['Día'][i].strftime('%A')+", " + df['Día'][i].strftime('%d de %B de %Y')
+    asunto = (f"""*Notificación sobre Pedido {df['Codigo_recepcion'][i]} {fecha_p}""")
         
-        em = MIMEMultipart("alternative")
-        em['From'] = email_usuario_e
-        em['To'] = email_encargado
-        em['Subject'] = asunto
+    em = MIMEMultipart("alternative")
+    em['From'] = email_usuario_e
+    em['To'] = email_encargado
+    em['Subject'] = asunto
         #em.set_content(cuerpo)
         
-        html = html_estructura(df,i)
+    html = html_estructura(df,i)
         
-        parte_html = MIMEText(html, "html")
-        em.attach(parte_html)
+    parte_html = MIMEText(html, "html")
+    em.attach(parte_html)
         
         #SSL seguridad = mantiene conexión segura ...
         # ... al enviar data entre dos sistemas
-        contexto = ssl.create_default_context()
+    contexto = ssl.create_default_context()
             # ------------  Enviar correo --------------
         # Define servidor, puerto y usar el contexto
         # para el envio se usa smtplib
         #se define el servidor, en este caso GMAIL
-        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-            smtp.starttls(context=contexto)
-            smtp.login(email_usuario_e, contraseña_usuario_e)
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls(context=contexto)
+        smtp.login(email_usuario_e, contraseña_usuario_e)
             #as_string se da el formato correcto
-            smtp.sendmail(email_usuario_e, email_encargado, em.as_string())
-            print("Mensaje enviado encargado")
+        smtp.sendmail(email_usuario_e, email_encargado, em.as_string())
+        print("Mensaje enviado encargado")
+        print(contador)
 #extraer los datos en variable del dataframe =>
 # => "Nombre”, “Teléfono”, “Código recepción”, “Total” y “Día”
 # ejecución del codigo para el envio del email
